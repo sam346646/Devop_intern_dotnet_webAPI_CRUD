6956ec92d4117c8d0c4f81e571c2809b2509f80b
sam346646 <sammanasseh310@gmail.com>
2023-05-04 12:50:03 +0530

    index updated

---
+++ webApplication/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+using webApplication.Models;
+
+namespace webApplication.Data;
+
+public class ContactsAPIDbContext: DbContext
+{
+    public ContactsAPIDbContext(DbContextOptions options):base(options)
+    {
+
+    }
+    public DbSet <Contact> Contacts {get; set; }
+    
+}
